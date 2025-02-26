a93d9892610a812b68c587e0555b05de5079cd15
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-25 22:46:37 -0300

    classe que manipula os erros gerados ao registrar uma despesa

---
+++ src/CashFlow.Exceptions/ExceptionsBase/ErrorOnValidateException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Exceptions.ExceptionsBase
+{
+    public class ErrorOnValidateException : CashFlowExceptions
+    {
+        public List<string> Errors { get; set; } = [];
+        public ErrorOnValidateException(List<string> errorMessages)
+        {
+            Errors = errorMessages;
+        }
+
+        public ErrorOnValidateException(string errorMessage)
+        {
+            Errors = [errorMessage];
+        }
+    }
+}
