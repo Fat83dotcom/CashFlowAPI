5228911872376a9b1db673e66629129833d7b0d9
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-25 22:33:10 -0300

    Validando o registrador de despesas

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpensesUseCase.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exceptions.ExceptionsBase;
+
+
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpensesUseCase
+    {
+        public ResponseRegisterExpanseJson Execute(RequestExpenseJson request)
+        {
+            Validate(request);
+            return new ResponseRegisterExpanseJson()
+            {
+                Title = request.Title
+            };
+        }
+
+        private void Validate(RequestExpenseJson request)
+        {
+            var validator = new RegisterExpenseValidator(request);
+            var result = validator.Validate(request);
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(fv => fv.ErrorMessage).ToList(); // Extrai as mensagens de texto dos objetos validation failure
+                throw new ErrorOnValidateException(errorMessages);
+            }
+        }
+    }
+}
