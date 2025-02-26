66d9aae7795cfdf53df0920d27372e80aeac4afc
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-25 22:29:39 -0300

    Criando filtro de exceções.

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exceptions.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CashFlow.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(ResponseRegisterExpanseJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody]RequestExpenseJson request)
+        {
+            try
+            {
+                var response = new RegisterExpensesUseCase().Execute(request);
+                return Created(string.Empty, response);
+            }
+            catch (ErrorOnValidateException e)
+            {
+                ResponseErrorJson error = new(e.Errors);
+        
+                return BadRequest(error);
+            }
+            catch (Exception)
+            {
+                ResponseErrorJson error = new("Erro desconhecido.");
+                
+                return BadRequest(error);
+            }
+        }
+    }
+}
