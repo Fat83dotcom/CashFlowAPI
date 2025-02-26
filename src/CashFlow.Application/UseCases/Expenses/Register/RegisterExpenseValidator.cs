961ac98c44cf26f5015806750b875bef3c1adca6
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-25 22:36:16 -0300

    Aplicando validações com fluent validator

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseValidator : AbstractValidator<RequestExpenseJson> // valida a requisição
+    {
+        public RegisterExpenseValidator(RequestExpenseJson request)
+        {
+            RuleFor(expense => expense.Title).NotEmpty().WithMessage("O titulo não pode estar vazio.");
+            RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("O valor deve ser maior que zero.");
+            RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("A data deve ser igual ou anterior a data atual.");
+            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage($"Tipo de pagamento invalido{request.PaymentType}");
+        }
+    }
+}
