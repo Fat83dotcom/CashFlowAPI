eb9d5c089463fca716c4a8ba1c122dcfc33009ba
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-25 22:30:17 -0300

    criando filtro de exceções

---
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Exceptions.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is CashFlowExceptions)
+            {
+                
+            }
+            else
+            {
+                
+            }
+
+        }
+
+        private void HandleProjectExeption(ExceptionContext context)
+        {
+
+        }
+
+        private void ThrowUnknowerror(ExceptionContext context)
+        {
+
+        }
+    }
+}
