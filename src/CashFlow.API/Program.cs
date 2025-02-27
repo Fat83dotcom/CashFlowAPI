674046cc1d94da449e88b855ca216ca49c295575
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-27 09:13:06 -0300

    Ativando o midlleware do .net

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -1,20 +1,24 @@
+using CashFlow.API.Filters;
+using CashFlow.API.MiddleWare;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMvc(o => o.Filters.Add<ExceptionFilter>());
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseMiddleware<CultureMiddleWare>();
 
 app.UseSwagger();
 
