d0e979821d7b2a3b5d8d905129336cee07e85a86
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-27 09:12:22 -0300

    Middleware para validar a linguagem do usuario

---
+++ src/CashFlow.API/MiddleWare/CultureMiddleWare.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.MiddleWare
+{
+    public class CultureMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        public CultureMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+            CultureInfo cultureInfo = new("pt-BR");
+
+            if (!string.IsNullOrWhiteSpace(culture))
+            {
+                cultureInfo = new CultureInfo(culture);
+            }
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+            await _next(context);
+        }
+    }
+}
