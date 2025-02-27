168dcfc2dc3c10bbd0044b0b0c1cb29f5eaac89b
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-27 09:11:39 -0300

    Implementando o tratamento de erro para a exceção erroronvalidateexception e para erros desconhecidos

--- src/CashFlow.API/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,6 @@
-﻿using CashFlow.Exceptions.ExceptionsBase;
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exceptions.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CashFlow.API.Filters
@@ -9,23 +11,35 @@ namespace CashFlow.API.Filters
         {
             if (context.Exception is CashFlowExceptions)
             {
-                
+                HandleProjectExeption(context);
             }
             else
             {
-                
+                ThrowUnknowerror(context);
             }
-
         }
 
         private void HandleProjectExeption(ExceptionContext context)
         {
-
+            if (context.Exception is ErrorOnValidateException e)
+            {
+                ResponseErrorJson errorResponse = new(e.Errors);
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new BadRequestObjectResult(errorResponse);
+            }
+            else
+            {
+                ResponseErrorJson errorResponse = new(context.Exception.Message);
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new BadRequestObjectResult(errorResponse);
+            }
         }
 
         private void ThrowUnknowerror(ExceptionContext context)
         {
-
+            ResponseErrorJson errorResponse = new(ResourceErrorMessages.UNKNOW_ERROR);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
         }
     }
 }
