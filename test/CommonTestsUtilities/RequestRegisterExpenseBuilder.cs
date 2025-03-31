62120c91600d9bab19f3aa2f334e6d367139952d
fat83dotcom <fernando.quimic@gmail.com>
2025-03-30 22:02:10 -0300

    🎯 Adiciona gerador de dados fictícios para testes automatizados 🏗️🧪
    ➕ Criada a classe RequestRegisterExpenseBuilder para facilitar a geração de objetos RequestExpenseJson nos testes.
    🎭 Utilização da biblioteca Bogus para gerar dados aleatórios realistas, reduzindo a necessidade de setups manuais.
    🔄 O RequestExpenseJsonFaker define regras para gerar títulos, descrições, datas e valores automaticamente.
    🚀 Essa implementação melhora a manutenção, a reutilização de código e garante testes mais dinâmicos!

---
+++ test/CommonTestsUtilities/RequestRegisterExpenseBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestsUtilities
+{
+    public class RequestRegisterExpenseBuilder
+    {
+        public RequestExpenseJson Build()
+        {
+            RequestExpenseJsonFaker requestExpenseFaker = new();
+            return requestExpenseFaker.Generate();   
+        }
+    }
+}
+
+public class RequestExpenseJsonFaker : Faker<RequestExpenseJson>
+{
+    public RequestExpenseJsonFaker()
+    {
+        RuleFor(r => r.Title, f => f.Name.JobTitle());
+        RuleFor(r => r.Description, f => f.Rant.Review());
+        RuleFor(r => r.Date, f => f.Date.Between(DateTime.Now.AddDays(-30), DateTime.Now));
+        RuleFor(r => r.Amount, f => f.Random.Number(0, 5000));
+        RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
