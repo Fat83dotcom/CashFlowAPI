026c7aead0cef2fc3fcf98dce0c46eaf728c0896
fat83dotcom <fernando.quimic@gmail.com>
2025-03-30 22:22:43 -0300

    🔧 Ajusta tipo do valor de Amount para Decimal nos testes 💰✅
    🔄 Substituído f.Random.Number(0, 5000) por f.Random.Decimal(0, 5000).
    📊 Garante maior precisão nos valores gerados para Amount, refletindo melhor os valores financeiros.
    🛠️ Melhoria na confiabilidade dos testes e na consistência dos dados! 🚀

--- test/CommonTestsUtilities/RequestRegisterExpenseBuilder.cs
+++ test/CommonTestsUtilities/RequestRegisterExpenseBuilder.cs
@@ -23,7 +23,7 @@ public class RequestExpenseJsonFaker : Faker<RequestExpenseJson>
         RuleFor(r => r.Title, f => f.Name.JobTitle());
         RuleFor(r => r.Description, f => f.Rant.Review());
         RuleFor(r => r.Date, f => f.Date.Between(DateTime.Now.AddDays(-30), DateTime.Now));
-        RuleFor(r => r.Amount, f => f.Random.Number(0, 5000));
+        RuleFor(r => r.Amount, f => f.Random.Decimal(0, 5000));
         RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>());
     }
 }
