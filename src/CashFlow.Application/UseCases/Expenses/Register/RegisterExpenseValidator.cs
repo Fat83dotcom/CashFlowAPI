919a2951af0cd74a66ebe18df07d32bb7b6ac50b
fat83dotcom <fernando.quimic@gmail.com>
2025-04-15 22:16:08 -0300

    ✅ Adiciona testes unitários para validações de cadastro de despesas
    - Cobertura para os cenários de título vazio, valor igual ou menor que zero, data futura e tipo de pagamento inválido
    - Ajuste na mensagem de erro de tipo de pagamento para não exibir o valor inválido no texto

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -11,7 +11,7 @@ namespace CashFlow.Application.UseCases.Expenses.Register
             RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_ERROR);
             RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.GREATER_THAN_ZERO);
             RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.DATE_LESS_OR_EQUAL_TO);
-            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage($"{ResourceErrorMessages.PAYMENT_ERROR} {request.PaymentType}");
+            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage($"{ResourceErrorMessages.PAYMENT_ERROR}");
         }
     }
 }
--- test/ValidatorsTests/Expenses/Register/RegisterExpensesValidatorTests.cs
+++ test/ValidatorsTests/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -3,6 +3,7 @@ using CashFlow.Communication.Requests;
 using CashFlow.Communication.Enums;
 using CommonTestsUtilities;
 using FluentAssertions;
+using CashFlow.Exceptions.ExceptionsBase;
 
 namespace ValidatorsTests.Expenses.Register
 {
@@ -12,18 +13,88 @@ namespace ValidatorsTests.Expenses.Register
         public void Success()
         {
             //Arrange
-
             RequestRegisterExpenseBuilder requestBuilder = new();
             var request = requestBuilder.Build();
             RegisterExpenseValidator validator = new(request);
             
             //Act
-
             var result = validator.Validate(request);
 
             //Assert
-
             result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ErrorTitleEmpty()
+        {
+            //Arrange
+            RequestRegisterExpenseBuilder requestBuilder = new();
+            var request = requestBuilder.Build();
+            RegisterExpenseValidator validator = new(request);
+            request.Title = string.Empty;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_ERROR));
+            // ContainSingle sem expressão lambda ferifica se a lista contem somente um elemento
+            // Com a expressão, ele verifica se existe a ocorrencia, independente do tamanho da lista, somente uma vez.
+        }
+
+        [Theory] // Permite que a função de teste receba parametros
+        [InlineData(0)] // Passa os parametros para a função
+        [InlineData(-1)] // Passa os parametros para a função
+        [InlineData(-2)] // Passa os parametros para a função
+        public void ErrorAmountEqualZero(decimal amount)
+        {
+            //Arrange
+            RequestRegisterExpenseBuilder requestBuilder = new();
+            var request = requestBuilder.Build();
+            RegisterExpenseValidator validator = new(request);
+            request.Amount = amount;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.GREATER_THAN_ZERO));
+        }
+
+        [Fact]
+        public void ErrorToFutureDate()
+        {
+            //Arrange
+            RequestRegisterExpenseBuilder requestBuilder = new();
+            var request = requestBuilder.Build();
+            RegisterExpenseValidator validator = new(request);
+            request.Date = DateTime.Now.AddDays(1);
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.DATE_LESS_OR_EQUAL_TO));
+        }
+
+        [Fact]
+        public void ErrorToPayment()
+        {
+            //Arrange
+            RequestRegisterExpenseBuilder requestBuilder = new();
+            var request = requestBuilder.Build();
+            RegisterExpenseValidator validator = new(request);
+            request.PaymentType = (PaymentType)15;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_ERROR));
         }
     }
 }
