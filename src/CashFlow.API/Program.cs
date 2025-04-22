164365e49b857301b46bfb3ccb3508e69ab3b08f
fat83dotcom <fernando.quimic@gmail.com>
2025-04-22 00:11:08 -0300

    🛠️ Adicionada injeção de dependência da infraestrutura
    Importado o namespace CashFlow.Infraestructure.
    
    Registrada extensão AddInfrastructure() para configurar os serviços da camada de infraestrutura.
    💡 Isso centraliza e organiza a configuração de serviços externos como bancos de dados.

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -1,5 +1,7 @@
 using CashFlow.API.Filters;
 using CashFlow.API.MiddleWare;
+using CashFlow.Infraestructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMvc(o => o.Filters.Add<ExceptionFilter>());
+ 
+builder.Services.AddInfrastructure(); // Extensão para a injeção de dependencia "DependencyInjectionExtension"
+ 
 
 var app = builder.Build();
 
