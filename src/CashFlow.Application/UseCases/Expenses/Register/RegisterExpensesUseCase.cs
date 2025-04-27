1c23c1324cf6c75af2465ae420c9d332c1e6e424
fat83dotcom <fernando.quimic@gmail.com>
2025-04-27 20:59:27 -0300

    🏗️ Implementado a interface IRegisterExpensesUseCase.
    🧠 Alterado o RegisterExpensesUseCase para utilizar IExpenseRepository na persistência de dados.
    
    ➕ Adicionado criação da entidade Expense a partir da requisição.
    
    💾 Persistido a despesa no banco de dados usando o repositório.
    
    🔄 Alterado retorno da execução para enviar o Id da despesa criada, ao invés do título.

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpensesUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpensesUseCase.cs
@@ -1,26 +1,44 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Application.UseCases.Expenses.Register.Interfaces;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exceptions.ExceptionsBase;
-
 
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
-    public class RegisterExpensesUseCase
+    public class RegisterExpensesUseCase : IRegisterExpensesUseCase
     {
-        RegisterExpensesUseCase()
-        {
+        private readonly IExpenseRepository _repository;
 
+        public RegisterExpensesUseCase()
+        {
         }
+
+        public RegisterExpensesUseCase(IExpenseRepository respository)
+        {
+            _repository = respository;
+        }
+
         public ResponseRegisterExpanseJson Execute(RequestExpenseJson request)
         {
             Validate(request);
 
+            var entity = new Expense
+            {
+                Amount = request.Amount,
+                Date = request.Date,
+                Description = request.Description,
+                Title = request.Title,
+                PaymentType = (Domain.Enums.PaymentType)request.PaymentType
+            };
 
+            _repository.Save(entity);
 
             return new ResponseRegisterExpanseJson()
             {
-                Title = request.Title
+                Id = entity.Id
             };
         }
 
