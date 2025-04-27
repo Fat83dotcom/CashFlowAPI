b4ced6aa51761c8d2c412a395d05a258d8a1b719
fat83dotcom <fernando.quimic@gmail.com>
2025-04-27 20:55:25 -0300

    🔥 Removido import de filtros e exceções não utilizados.
    🔄 Alterado parâmetro do método Register para receber IRegisterExpensesUseCase via injeção de dependência.
    
    ➕ Adicionado novo endpoint GetExpenses para futura listagem de despesas (por enquanto retornando valor fixo 5).

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,8 +1,6 @@
-﻿using CashFlow.API.Filters;
-using CashFlow.Application.UseCases.Expenses.Register;
+﻿using CashFlow.Application.UseCases.Expenses.Register.Interfaces;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using CashFlow.Exceptions.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -14,11 +12,20 @@ namespace CashFlow.API.Controllers
     {
         [HttpPost("register")]
         [ProducesResponseType(typeof(ResponseRegisterExpanseJson), StatusCodes.Status201Created)]
-        public IActionResult Register([FromBody]RequestExpenseJson request)
+        public IActionResult Register([FromBody]RequestExpenseJson request, [FromServices] IRegisterExpensesUseCase useCase)
         {
-            var response = new RegisterExpensesUseCase().Execute(request);
+            var response = useCase.Execute(request);
         
             return Created(string.Empty, response);
+        }
+
+        [HttpGet("getExpenses")]
+        [ProducesResponseType(typeof(ResposnseGetExpensesJson), StatusCodes.Status200OK)]
+        public IActionResult GetExpenses()
+        {
+            var response = 5;
+
+            return Ok(response);
         }
     }
 }
