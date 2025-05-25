dc149b40468d065cce36be021919ede39a7c1ebe
fat83dotcom <fernando.quimic@gmail.com>
2025-05-25 19:06:21 -0300

    🔧 Refatorando a classe de injeção de dependencias

--- src/CashFlow.Infraestructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infraestructure/DependencyInjectionExtension.cs
@@ -11,12 +11,22 @@ namespace CashFlow.Infraestructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration) // Ao passar this é criado um metodo de extensão de Iservicecollection
         {
+            AddDBcontext(services, configuration);
+            AddRepositories(services);
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+        }
+
+        private static void AddDBcontext(IServiceCollection services, IConfiguration configuration)
+        {
             var connectionString = configuration.GetConnectionString("mySql");
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 42));
 
             services.AddDbContext<CashFlowDbContext>(options =>
                 options.UseMySql(connectionString, serverVersion));
-            services.AddScoped<IExpenseRepository, ExpenseRepository>();
         }
     }
 }
