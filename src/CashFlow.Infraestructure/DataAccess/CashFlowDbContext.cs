026ad253f337bdd4afc86e16775095be9ca8cce1
fat83dotcom <fernando.quimic@gmail.com>
2025-05-25 19:05:06 -0300

    🔧Refatorando CashFlowDbContex

--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -6,23 +6,10 @@ namespace CashFlow.Infraestructure.DataAccess
 {
     public class CashFlowDbContext : DbContext
     {
-        private readonly string? _ConnectString;
+
+        public CashFlowDbContext(DbContextOptions options) : base(options) {}
 
         public DbSet<Expense> Expenses { get; set; }
 
-        public CashFlowDbContext(IConfiguration configuration)
-        {
-            _ConnectString = configuration.GetConnectionString("mySql");
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var serverVersion = new MySqlServerVersion(new Version(8, 0, 42));
-
-            if (_ConnectString is not null)
-            {
-                optionsBuilder.UseMySql(_ConnectString, serverVersion); 
-            }
-        }
     }
 }
